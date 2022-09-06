54ae181c5be96d649e5296d4b0751ec03a103099
Duane <lausellduane@gmail.com>
2022-09-05 21:00:02 -0400

    feat: add react-query client provider and devtools

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import '@patternfly/react-core/dist/styles/base.css'
 import PageLayoutDefaultNav from './components/nav_bar/nav_bar';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PageLayoutDefaultNav />
+        <QueryClientProvider client={queryClient}>
+          <PageLayoutDefaultNav />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </div>
     );
   }
