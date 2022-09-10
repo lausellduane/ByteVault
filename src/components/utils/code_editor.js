0d1125f06d2514b3b4483a353fb3b33472832457
lausellduane <lausellduane@gmail.com>
2022-09-10 16:09:34 -0400

    refactor: changed code editor wrapper class component to function component

--- src/components/utils/code_editor.js
+++ src/components/utils/code_editor.js
@@ -1,54 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CodeEditor, Language } from '@patternfly/react-code-editor';
 
-export default class BasicCodeEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isDarkTheme: false,
-      isLineNumbersVisible: true,
-      isReadOnly: false,
-      isMinimapVisible: true,
-      language: props.language?.id === 1 ? Language.javascript : 
-                props.language?.id === 2 ? Language.python :
-                props.language?.id === 3 ? Language.go :
-                Language.plaintext
-    };
-    
-    this.toggleReadOnly = checked => {
-      this.setState({
-        isReadOnly: checked
-      });
-    };
-    
-    this.onEditorDidMount = (editor, monaco) => {
-      editor.layout();
-      editor.focus();
-      monaco.editor.getModels()[0].updateOptions({ tabSize: 5 });
-    };
+export const ByteVaultCodeEditor = (props) => {
+  const { isReadOnly, value, handleSnippetInputChange, language } = props;
+  const [formattedLanguage, setFormattedLanguage] = useState('');
+
+  const onEditorDidMount = (editor, monaco) => {
+    editor.layout();
+    editor.focus();
+    monaco.editor.getModels()[0].updateOptions({ tabSize: 5 });
+    const lang = language?.id === 1 ? Language.javascript : 
+        language?.id === 2 ? Language.python :
+        language?.id === 3 ? Language.go :
+        Language.plaintext
+    setFormattedLanguage(lang);
   }
-  
-  render() {
-    const { isDarkTheme, isLineNumbersVisible, isReadOnly, isMinimapVisible, language } = this.state;
-    const { value, handleSnippetInputChange } = this.props;
-    
-    return (
-      <>
-        <CodeEditor
-          isCopyEnabled  // keep an eye on copy getting cut before escaped characters
-          isDownloadEnabled
-          isDarkTheme={isDarkTheme}
-          isLineNumbersVisible={isLineNumbersVisible}
-          isReadOnly={isReadOnly}
-          isMinimapVisible={isMinimapVisible}
-          isLanguageLabelVisible={language?.id}
-          code={value}
-          onChange={handleSnippetInputChange}
-          language={language}
-          onEditorDidMount={this.onEditorDidMount}
-          height='400px'
-        />
-      </>
-    );
-  }
-}
+
+  return (
+    <>
+      <CodeEditor
+        isCopyEnabled  // keep an eye on copy getting cut before escaped characters
+        isDownloadEnabled
+        isReadOnly={isReadOnly}
+        isLanguageLabelVisible={language?.id}
+        code={value}
+        onChange={handleSnippetInputChange}
+        language={formattedLanguage}
+        onEditorDidMount={onEditorDidMount}
+        height='400px'
+      />
+    </>
+  );
+};
